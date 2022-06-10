9ced8089bbb908e0404b1c7845bda7e4501a94aa
Dineshkumar-2001 <dineshkumar@valorpaytech.com>
2022-06-10 16:14:41 +0530

    ok

--- src/test.js
+++ src/test.js
@@ -4,8 +4,8 @@ export default class test extends Component {
   render() {
     return (
       <div>
-          <h1>1</h1>
-          <h1>2</h1>
+          <h1>3</h1>
+          <h1>4</h1>
       </div>
     )
   }
